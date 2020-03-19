72100900d7dcf85934fd5931cf60447eee1ef410
PietjeGeluk <62058850+PietjeGeluk@users.noreply.github.com>
2020-03-19 08:20:13 +0100

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8
+{
+    public class Car
+    {
+        public string brand;
+        public string model;
+        public int year;
+
+        public Car()
+        {
+           model = "316";    
+           year = 2014;
+        }
+    }
+}
